55a28a844e032502d1979629b33bd03bd9b2f42a
大川雄生 <OkawaUki@okawauki.local>
2015-08-07 16:47:31 +0900

    masked for hands-on

--- PushNotificationSample/AppDelegate.m
+++ PushNotificationSample/AppDelegate.m
@@ -9,8 +9,6 @@
 #import "AppDelegate.h"
 #import <NCMB/NCMB.h>
 
-#import <CoreTelephony/CTTelephonyNetworkInfo.h>
-#import <CoreTelephony/CTCarrier.h>
 
 @interface AppDelegate ()
 
@@ -23,41 +21,25 @@
     // Override point for customization after application launch.
     
     //SDKの初期化
-    [NCMB setApplicationKey:@"YOUR_APPLICATION_EKY"
+    [NCMB setApplicationKey:@"YOUR_APPLICATIONs_EKY"
                   clientKey:@"YOUR_CLIENT_KEY"];
     
     //Interactive Notifications用に承認するアクションを作成
-    UIMutableUserNotificationAction *acceptAction =
-    [[UIMutableUserNotificationAction alloc] init];
-    acceptAction.identifier = @"ACCEPT_IDENTIFIER";
-    acceptAction.title = @"Accept";
-    acceptAction.activationMode = UIUserNotificationActivationModeBackground;
+
     //Interactive Notifications用に拒否アクションを作成
-    UIMutableUserNotificationAction *denyAction =
-    [[UIMutableUserNotificationAction alloc] init];
-    denyAction.identifier = @"DENY_IDENTIFIER";
-    denyAction.title = @"Deny";
-    denyAction.activationMode = UIUserNotificationActivationModeBackground;
+
     
     //カテゴリを作成
-    UIMutableUserNotificationCategory *friendReqCategory =
-    [[UIMutableUserNotificationCategory alloc] init];
+
     //プッシュ通知で指定するカテゴリ名を設定
-    friendReqCategory.identifier = @"FRIEND_REQUEST";
+
     //表示するアクションを設定
-    [friendReqCategory setActions:@[acceptAction, denyAction]
-                       forContext:UIUserNotificationActionContextDefault];
+
     
     //カテゴリを集めたNSSetを作成
-    NSSet *categories = [NSSet setWithObject:friendReqCategory];
+
     
     //プッシュ通知のタイプを設定して許可画面を表示する
-    UIUserNotificationType types = UIUserNotificationTypeBadge |
-    UIUserNotificationTypeSound |
-    UIUserNotificationTypeAlert;
-    UIUserNotificationSettings *mySettings =
-    [UIUserNotificationSettings settingsForTypes:types categories:categories];
-    [[UIApplication sharedApplication] registerUserNotificationSettings:mySettings];
     
     //デバイストークンの要求
     [[UIApplication sharedApplication] registerForRemoteNotifications];
@@ -70,12 +52,9 @@
 handleActionWithIdentifier: (NSString *) identifier
 forRemoteNotification: (NSDictionary *) notification
   completionHandler: (void (^)()) completionHandler {
+    
     //アクションのIdnetifierごとに処理を分けて実装する
-    if ([identifier isEqualToString: @"ACCEPT_IDENTIFIER"]) {
-        [self handleAcceptActionWithNotification];
-    } else {
-        [self handleDenyActionWithNotification];
-    }
+
     //ハンドリングが終了する場合に必ず以下を実行する
     completionHandler();
 }
@@ -98,9 +77,6 @@ didRegisterForRemoteNotificationsWithDeviceToken:(NSData *)deviceToken
     [installation setDeviceTokenFromData:deviceToken];
     
     //キャリアを取得して端末情報に登録
-    CTTelephonyNetworkInfo *netinfo = [[CTTelephonyNetworkInfo alloc] init];
-    CTCarrier *carrier = [netinfo subscriberCellularProvider];
-    [installation setObject:carrier.carrierName forKey:@"carrier"];
     
     //ニフティクラウド mobile  backendのデータストアに登録
     [installation saveInBackgroundWithBlock:^(NSError *error) {
@@ -119,23 +95,7 @@ didRegisterForRemoteNotificationsWithDeviceToken:(NSData *)deviceToken
 didReceiveRemoteNotification:(NSDictionary *)userInfo
 fetchCompletionHandler:(void (^)(UIBackgroundFetchResult))completionHandler
 {
-    //mobile backendのコンテンツを取得
-    NCMBQuery *query = [NCMBQuery queryWithClassName:@"contents"];
-    [query findObjectsInBackgroundWithBlock:^(NSArray *objects, NSError *error) {
-        if (error){
-            NSLog(@"search error:%@", error);
-            completionHandler(UIBackgroundFetchResultNoData);
-        } else {
-            //このあとここに処理を書いていきます
-            
-            for (NCMBObject *obj in objects) {
-                NSString *title = [obj objectForKey:@"title"];
-                NSString *url = [obj objectForKey:@"url"];
-                NSLog(@"title:%@, url:%@", title, url);
-            }
-            completionHandler(UIBackgroundFetchResultNewData);
-        }
-    }];
+    
 }
 
 - (void)applicationWillResignActive:(UIApplication *)application {
